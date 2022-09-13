bac6d5536ada87adb6eb4c6b0b0fb4ea4fa8f1c8
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-09-13 13:03:23 -0600

    feat: routes for CRUD on categories tables

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,85 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// get all categories in the DB
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.findAll({
+      include: [
+        {
+          model: Product,
+        },
+      ],
+    });
+    res.json(categories);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// find one by id
+router.get('/:id', async (req, res) => {
+  try {
+    const oneCategory = await Category.findOne({
+      include: [
+        {
+          model: Product,
+        },
+      ],
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(oneCategory);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// create new category
+router.post('/', async (req, res) => {
+  try {
+    const newCategory = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.json(newCategory);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// updated a category based on it ID
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedCategory = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.json(updatedCategory);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+// delete category based on ID
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deletedCategory = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(deletedCategory);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 module.exports = router;
